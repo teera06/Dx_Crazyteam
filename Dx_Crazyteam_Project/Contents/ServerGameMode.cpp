8d031131b90c7ca5c86c621225641219c17f57f8
kky <kykim9050@naver.com>
2024-05-14 16:25:07 +0900

    김경윤 : 에러처리

--- Dx_Crazyteam_Project/Contents/ServerGameMode.cpp
+++ Dx_Crazyteam_Project/Contents/ServerGameMode.cpp
@@ -46,8 +46,7 @@ void AServerGameMode::BeginPlay()
 
 	PlayLobby = GetWorld()->SpawnActor<APlayLobby>("PlayLobby");
 	TitleMenu = GetWorld()->SpawnActor<ATitleMenu>("TitleMenu");
-
-	TitleMenu->GetFunction();
+	//TitleMenu->SetFunction(std::bind(&AServerGameMode::CollectWindowAppear, this));
 }
 
 void AServerGameMode::Tick(float _DeltaTime)
