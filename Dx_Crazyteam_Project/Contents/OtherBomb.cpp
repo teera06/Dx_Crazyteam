7416031f11f005997a01f698a8243a1dc18f0e7e
rhojingoo <rhojingoo@naver.com>
2024-05-14 17:58:47 +0900

    노진구 :: WaterOtherBomb, Packek, NetInterFACE 업데이트

---
+++ Dx_Crazyteam_Project/Contents/OtherBomb.cpp
@@ -0,0 +1,75 @@
+#include "PreCompile.h"
+#include "OtherBomb.h"
+#include <EngineCore/Collision.h>
+#include <EngineCore/SpriteRenderer.h>
+#include <EngineCore/DefaultSceneComponent.h>
+#include "Game_Core.h"
+#include "Packets.h"
+
+
+AOtherBomb::AOtherBomb()
+{
+	Root = CreateDefaultSubObject<UDefaultSceneComponent>("RendererRoot");
+	SetRoot(Root);
+	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
+	Renderer->SetAutoSize(0.05f, true);
+	Renderer->SetupAttachment(Root);
+	Renderer->SetOrder(5);
+	SetActorScale3D(FVector(20, 20, 1));
+}
+
+AOtherBomb::~AOtherBomb()
+{
+}
+
+void AOtherBomb::BeginPlay()
+{
+	AActor::BeginPlay();
+}
+
+void AOtherBomb::Tick(float _DeltaTime)
+{
+	AActor::Tick(_DeltaTime);
+
+	std::shared_ptr<UEngineProtocol> Protocol = nullptr;
+
+	do
+	{
+		Protocol = PopProtocol();
+		if (nullptr == Protocol)
+		{
+			break;
+		}
+
+		EContentPacket PacketType = Protocol->GetPacketType<EContentPacket>();
+
+		switch (PacketType)
+		{
+		case WaterBombUpdatePacket:
+		{
+			std::shared_ptr<UWaterBombUpdatePacket> UpdatePacket = std::dynamic_pointer_cast<UWaterBombUpdatePacket>(Protocol);
+
+			SetActorLocation(UpdatePacket->Pos);
+
+			std::string SpriteNames = UpdatePacket->SpriteName;
+
+
+
+			int AnimationInFO = UpdatePacket->AnimationInfo;
+
+			if (SpriteNames != "")
+			{
+				Renderer->SetSprite(SpriteNames, AnimationInFO);
+			}
+			int a = 0;
+			break;
+		}
+		default:
+			break;
+		}
+
+	} while (nullptr != Protocol);
+
+
+}
+
