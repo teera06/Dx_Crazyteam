d9df3c816ee19fe91fb1749e4b12ad46e32fe5b2
rhojingoo <rhojingoo@naver.com>
2024-05-14 10:06:39 +0900

    노진구 :: Otherplayer Update

--- Dx_Crazyteam_Project/Contents/OtherPlayer.h
+++ Dx_Crazyteam_Project/Contents/OtherPlayer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <EngineCore/Actor.h>
 #include <EngineBase/NetObject.h>
+#include <EngineCore/DefaultSceneComponent.h>
 
 // Ό³Έν :
 class AOtherPlayer : public AActor, public UNetObject
@@ -20,7 +21,15 @@ protected:
 	void BeginPlay() override;
 	void Tick(float _DeltaTime) override;
 
-private:
+	void IdleStart();
+	void Idle(float _DeltaTime);
 
+	void MoveStart();
+	void Move(float _DeltaTime);
+
+private:
+	UStateManager State;
+	UDefaultSceneComponent* Root = nullptr;
+	USpriteRenderer* Renderer = nullptr;
 };
 
