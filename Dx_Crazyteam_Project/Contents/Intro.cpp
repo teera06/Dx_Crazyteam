1e91dd60f0205160ed2556a3e918f46b5486d45c
soyou088 박성우 <soyou088@gmail.com>
2024-05-14 16:32:49 +0900

    박성우 : 로비 업데이트

--- Dx_Crazyteam_Project/Contents/Intro.cpp
+++ Dx_Crazyteam_Project/Contents/Intro.cpp
@@ -4,6 +4,7 @@
 
 AIntro::AIntro()
 {
+
 }
 
 AIntro::~AIntro()
@@ -19,12 +20,16 @@ void AIntro::BeginPlay()
 	IntroUI->CreateAnimation("IntroNexen", "IntroUser", 0.1f, false, 20, 92);
 	IntroUI->SetAutoSize(1.0f, true);
 	IntroUI->AddToViewPort(10);
-	IntroUI->SetActive(true);
+	IntroUI->SetActive(false);
 
 	
 	IntroUI->ChangeAnimation("IntroUser");
 
-	 
+	StartUI = CreateWidget<UImage>(GetWorld(), "StartUI");
+	StartUI->SetSprite("first_bge.png");
+	StartUI->SetAutoSize(1.0f, true);
+	StartUI->AddToViewPort(10);
+	StartUI->SetActive(false);
 
 
 }
@@ -36,8 +41,14 @@ void AIntro::Tick(float _DeltaTime)
 	if (true == IntroUI->IsCurAnimationEnd())
 	{
 		IntroUI->ChangeAnimation("IntroNexen");
-
+		if (true == IntroUI->IsCurAnimationEnd())
+		{
+			IntroUI->SetActive(false);
+		
+			//StartUI->SetActive(true);
+		}
 	}
+	
 
 
 }
