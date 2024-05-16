194b838a066f686d0b140782220ae840cd24955e
KMJ <mjya8928@naver.com>
2024-05-16 14:43:04 +0900

    김명지 : 아이템 내용 수정 코어에 리소스 컷팅 추가

--- Dx_Crazyteam_Project/Contents/Item.cpp
+++ Dx_Crazyteam_Project/Contents/Item.cpp
@@ -5,7 +5,9 @@ AItem::AItem()
 {
 	FrontRenderer = CreateDefaultSubObject<USpriteRenderer>("ItemRender");
 	FrontRenderer->SetupAttachment(Root);
-	FrontRenderer->SetOrder(ERenderOrder::Item);
+
+	BackRenderer = CreateDefaultSubObject<USpriteRenderer>("ItemShadowRender");
+	BackRenderer->SetupAttachment(Root);
 }
 
 AItem::~AItem()
@@ -15,6 +17,16 @@ AItem::~AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+
+	FrontRenderer->SetOrder(ERenderOrder::Item);
+
+	BackRenderer->CreateAnimation("ItemShadow", "ItemShadow.png", 0.9f, true, 0, 1);
+	BackRenderer->ChangeAnimation("ItemShadow");
+	BackRenderer->SetAutoSize(0.6f, true);
+	BackRenderer->AddPosition(FVector::Down * 65.f);
+	BackRenderer->SetOrder(ERenderOrder::Shadow);
+	//BackRenderer->SetMulColor(FVector(1.f, 1.f, 1.f, 0.6f));
+
 }
 
 void AItem::Tick(float _DeltaTime)
