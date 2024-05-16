a1e2541ba02d0698680f6eb68dc9cada6052cdc4
rhojingoo <rhojingoo@naver.com>
2024-05-16 13:55:43 +0900

    노진구 :: 테스트

--- Dx_Crazyteam_Project/EngineBase/EngineNet.cpp
+++ Dx_Crazyteam_Project/EngineBase/EngineNet.cpp
@@ -106,6 +106,12 @@ void UEngineNet::RecvThreadFunction(USession* _Session, UEngineNet* _Net)
 			std::shared_ptr<UEngineProtocol> Protocal = Dis.ConvertProtocol(Protocol.GetPacketType(), Ser);
 			Dis.ProcessPacket(Protocal);
 
+			if (Ser.BufferSize() == Ser.GetReadOffset())
+			{
+				Ser.DataToReadOffsetPush();
+				break;
+			}
+
 			int Size = *(reinterpret_cast<int*>(Ser.DataCharPtrToWriteOffset()));
 			int WriteOffset = Ser.GetWriteOffset();
 			int ReadOffset = Ser.GetReadOffset();
@@ -125,6 +131,7 @@ void UEngineNet::RecvThreadFunction(USession* _Session, UEngineNet* _Net)
 				Ser.DataToReadOffsetPush();
 				break;
 			}
+
 
 		} 
 	}
