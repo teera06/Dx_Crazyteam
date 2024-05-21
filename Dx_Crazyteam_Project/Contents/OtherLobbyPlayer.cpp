b5aeb666a7739aa1b0c6b76061d68cbcd966d7f9
rhojingoo <rhojingoo@naver.com>
2024-05-21 17:21:02 +0900

    노진구 :: 업데이트

--- Dx_Crazyteam_Project/Contents/OtherLobbyPlayer.cpp
+++ Dx_Crazyteam_Project/Contents/OtherLobbyPlayer.cpp
@@ -9,10 +9,27 @@ AOtherLobbyPlayer::AOtherLobbyPlayer()
 
 	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
 	Renderer->SetupAttachment(Root);
-
 	Renderer->SetAutoSize(0.05f, true);
 	Renderer->SetOrder(5);
 	SetActorScale3D(FVector(20, 20, 1));
+
+
+	ImageRenderer = CreateWidget<UImage>(GetWorld(), "Room1");
+	//Room1->CreateAnimation("UnHover", "Room_0.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("Hover", "Room_1.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("Down", "Room_2.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("Up", "RoomX_0.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("XUnHover", "RoomX_0.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("XHover", "RoomX_1.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("XDown", "RoomX_2.png", 0.1f, false, 0, 0);
+	//Room1->CreateAnimation("XUp", "Room_0.png", 0.1f, false, 0, 0);
+	//Room1->AddToViewPort(12);
+	//Room1->SetSprite("Room_0.png");
+	//ImageRenderer->SetAutoSize(1.0f, true);
+	//ImageRenderer->SetPosition(FVector(-218.f, 157.f));
+	//ImageRenderer->SetActive(true);
+
+
 }
 
 AOtherLobbyPlayer::~AOtherLobbyPlayer()
