ae5827d2d565838f161f04dc4a4fac3e37956b11
yndoo 배연두 <yndoo2k@gmail.com>
2024-05-21 12:46:57 +0900

    배연두 : 탈것 애니메이션 완료

--- Dx_Crazyteam_Project/Contents/Player.h
+++ Dx_Crazyteam_Project/Contents/Player.h
@@ -170,7 +170,9 @@ private:
 	float DieAnimationTime = 2.f;
 	float DieTwinkleTime = 0.1f;
 
-	float RideGodModeTime = 3.f; // 탈것에 타거나 내릴 때 몇초간 무적모드
+	float RideGodModeTime = 2.f; // 탈것에 타거나 내릴 때 몇초간 무적모드
+	float RideTwinkleTime = 0.1f;
+	int RideAniTwinkleActive = 0;
 
 	std::shared_ptr<APlayer_Shadow> Shadow = nullptr;
 
