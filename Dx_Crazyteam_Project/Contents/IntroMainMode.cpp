fa7d540f93c5edee7b86e0231d08dcfa85101bbb
ChoiYeongHwan <cyh5343@naver.com>
2024-05-21 11:11:12 +0900

    최영환 : IntroMainMode에 Intro actor 추가

--- Dx_Crazyteam_Project/Contents/IntroMainMode.cpp
+++ Dx_Crazyteam_Project/Contents/IntroMainMode.cpp
@@ -1,5 +1,7 @@
 #include "PreCompile.h"
 #include "IntroMainMode.h"
+#include "Intro.h"
+#include "TitleMenu.h"
 
 AIntroMainMode::AIntroMainMode()
 {
@@ -13,17 +15,18 @@ void AIntroMainMode::BeginPlay()
 {
 	Super::BeginPlay();
 
+	Intro = GetWorld()->SpawnActor<AIntro>("Intro");
 }
 
 void AIntroMainMode::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
-
 }
 
 void AIntroMainMode::LevelEnd(ULevel* _NextLevel)
 {
 	Super::LevelEnd(_NextLevel);
+	Intro->Destroy();
 }
 
 void AIntroMainMode::LevelStart(ULevel* _PrevLevel)
