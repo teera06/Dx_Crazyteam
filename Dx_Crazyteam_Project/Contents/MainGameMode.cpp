10db4d061318b5428c97d1d8e280f232d4981aa8
김관호 김관호 <asdf36597@naver.com>
2024-05-23 16:37:12 +0900

    김관호 : 플레이어 생성위치 ConstValue 수정

--- Dx_Crazyteam_Project/Contents/MainGameMode.cpp
+++ Dx_Crazyteam_Project/Contents/MainGameMode.cpp
@@ -83,7 +83,7 @@ void AMainGameMode::LevelStart(ULevel* _PrevLevel)
 				MainPlayer->SetCurGameMode(this);
 				SetMainPlayer(MainPlayer);
 
-				FVector InitPos = GetCurMap()->PointToPos(ConstValue::VillageStartPos[UGame_Core::Net->GetSessionToken()]);
+				FVector InitPos = GetCurMap()->PointToPos(ConstValue::VillageStartPOINT[UGame_Core::Net->GetSessionToken()]);
 				MainPlayer->SetActorLocation(InitPos);
 
 				// 여기에서 메인 플레이어한테 번호를 하나 줄겁니다.
@@ -126,7 +126,7 @@ void AMainGameMode::LevelStart(ULevel* _PrevLevel)
 				MainPlayer->SetCurGameMode(this);
 				SetMainPlayer(MainPlayer);
 
-				FVector InitPos = GetCurMap()->PointToPos(ConstValue::VillageStartPos[UGame_Core::Net->GetSessionToken()]);
+				FVector InitPos = GetCurMap()->PointToPos(ConstValue::VillageStartPOINT[UGame_Core::Net->GetSessionToken()]);
 				MainPlayer->SetActorLocation(InitPos);
 
 				MainPlayer->SetObjectToken(UGame_Core::Net->GetSessionToken() * 1000);
